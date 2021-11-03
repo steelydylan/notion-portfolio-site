01043ea538e7fa68efbb800025ab8553fd452ac1
steelydylan <ess_president@me.com>
2021-11-03 19:38:39 +0900

    fix: trivial fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
-import { Header } from './components/Header'
-import { Footer } from './components/Footer'
+import { Header } from '../components/Header'
+import { Footer } from '../components/Footer'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (<>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { Portfolio } from './components/Portfolio'
+import { Portfolio } from '../components/Portfolio'
 
 const Home: NextPage = () => {
   return (
