17a7c8bddec652c1e5233ee06a580a240f0d72f1
steelydylan <ess_president@me.com>
2021-11-03 19:30:41 +0900

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<>
+    <Header />
+    <Component {...pageProps} />
+    <Footer />
+  </>)
 }
 export default MyApp
