39ca723b7407c117aed0f03ef8c70d9a2386cba2
steelydylan <ess_president@me.com>
2021-11-06 23:01:17 +0900

    fix(slug): trivial fix

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -22,7 +22,13 @@ export const Text = ({ text }) => {
         ].join(' ')}
         style={color !== 'default' ? { color } : {}}
       >
-        {text.link ? <a href={text.link.url}>{text.content}</a> : text.content}
+        {text.link ? (
+          <a href={text.link.url} className="text-indigo-500 hover:underline">
+            {text.content}
+          </a>
+        ) : (
+          text.content
+        )}
       </span>
     )
   })
