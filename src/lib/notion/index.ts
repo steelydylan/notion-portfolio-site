e53de53462ba3f8177115077b0082259ef8c948d
steelydylan <ess_president@me.com>
2021-11-06 18:02:07 +0900

    fix(notion): trivial fix

--- src/lib/notion/index.ts
+++ src/lib/notion/index.ts
@@ -29,24 +29,25 @@ export const getDatabase = async (databaseId: string) => {
       id: result.id,
     }
     Object.keys(d).forEach((key) => {
-      if (d[key].type === 'people') {
-        item[key.toLowerCase()] = d[key].people.map((p) => p.name)
-      } else if (d[key].type === 'rich_text') {
-        item[key.toLowerCase()] = d[key].rich_text[0].plain_text
-      } else if (d[key].type === 'files') {
-        if (d[key].files[0].type === 'external') {
-          item[key.toLowerCase()] = d[key].files[0].name
+      const property = d[key]
+      if (property.type === 'people') {
+        item[key.toLowerCase()] = property.people.map((p) => (p as any).name)
+      } else if (property.type === 'rich_text') {
+        item[key.toLowerCase()] = property.rich_text[0].plain_text
+      } else if (property.type === 'files') {
+        if (property.files[0].type === 'external') {
+          item[key.toLowerCase()] = property.files[0].name
         } else {
-          item[key.toLowerCase()] = d[key].files[0].file?.url
+          item[key.toLowerCase()] = property.files[0].file?.url
         }
-      } else if (d[key].type === 'title') {
-        item[key.toLowerCase()] = d[key].title[0].plain_text
-      } else if (d[key].type === 'checkbox') {
-        item[key.toLowerCase()] = d[key].checkbox
-      } else if (d[key].type === 'multi_select') {
-        item[key.toLowerCase()] = d[key].multi_select[0].name
-      } else if (d[key].type === 'date') {
-        item[key.toLowerCase()] = d[key].date.start
+      } else if (property.type === 'title') {
+        item[key.toLowerCase()] = property.title[0].plain_text
+      } else if (property.type === 'checkbox') {
+        item[key.toLowerCase()] = property.checkbox
+      } else if (property.type === 'multi_select') {
+        item[key.toLowerCase()] = property.multi_select[0].name
+      } else if (property.type === 'date') {
+        item[key.toLowerCase()] = property.date.start
       }
     })
     return item
