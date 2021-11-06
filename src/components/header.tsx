527bd806dc87c04a0bcf7108c0bee4a9b29b4be4
steelydylan <ess_president@me.com>
2021-11-06 18:13:14 +0900

    fix(header): trivial fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,7 +15,7 @@ export const Header = () => {
             </div>
 
             <div className="flex lg:hidden">
-              <button
+              {/* <button
                 type="button"
                 className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
                 aria-label="toggle menu"
@@ -26,7 +26,7 @@ export const Header = () => {
                     d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
                   ></path>
                 </svg>
-              </button>
+              </button> */}
             </div>
           </div>
 
