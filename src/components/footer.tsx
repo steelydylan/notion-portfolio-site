44af7104baf1cdc4035c7e13ef7a6a3ca37f8605
steelydylan <ess_president@me.com>
2021-11-14 00:18:05 +0900

    fix(footer): trivial fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,21 +3,6 @@ export const Footer = () => {
     <div className="bg-white pt-4 sm:pt-10 lg:pt-12">
       <footer className="max-w-screen-2xl px-4 md:px-8 mx-auto">
         <div className="flex flex-col items-center border-t pt-6">
-          <nav className="flex flex-wrap justify-center md:justify-start gap-x-4 gap-y-2 md:gap-6 mb-4">
-            <a
-              href="#"
-              className="text-gray-500 hover:text-indigo-500 active:text-indigo-600 transition duration-100"
-            >
-              Contact
-            </a>
-            <a
-              href="#"
-              className="text-gray-500 hover:text-indigo-500 active:text-indigo-600 transition duration-100"
-            >
-              Blog
-            </a>
-          </nav>
-
           <div className="flex gap-4">
             <a
               href="https://twitter.com/steelydylan"
